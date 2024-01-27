2ec1de56d16ac5621ffb62924fe9831a3dded877
Ahmed Elsherif <119296268+0xelsherif@users.noreply.github.com>
2024-01-27 04:59:40 +0200

    users create and dashboard

--- app.js
+++ app.js
@@ -6,37 +6,68 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+var moment = require('moment');
+
 
 mongoose
-  .connect('mongodb+srv://rookie:mcsTlC9CfcVai1up@cluster0.ufinlu3.mongodb.net/test', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`);
+    .connect('mongodb+srv://rookie:mcsTlC9CfcVai1up@cluster0.ufinlu3.mongodb.net/test', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err.message);
     });
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err.message);
-  });
 
 // auto refresh
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname, 'public'));
- 
- 
+
+
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
- 
+
 liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+// Models
+const Users = require("./models/usersSchema")
+
+
+
+// Get request
+app.get('/', (req, res) => {
+    res.render('index', { req: req });
+});
+
+app.get('/users', (req, res) => {
+    Users.find().then((allusers) => {
+        console.log(allusers);
+        res.render('users/users', { Array: allusers, moment: moment, req: req });
+    }).catch((error) => {
+        console.log(error)
+    });
+});
+
+// Post request
+app.post('/users/add', (req, res) => {
+    console.log(req.body)
+    Users.create(req.body)
+        .then(result => {
+            res.redirect("/users");
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
---
+++ models/usersSchema.js
@@ -0,0 +1,39 @@
+// usersSchema.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  gender: {
+    type: String,
+    enum: ['M', 'F'],
+  },
+
+},{ timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
